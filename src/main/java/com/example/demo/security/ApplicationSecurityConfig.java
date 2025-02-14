81e470e578578541870ec83da8347a00e9812a97
Manuel Agdon <manuel.salvador.agdon@easyequities.com.ph>
2025-02-14 19:55:55 +0800

    initial security config

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,24 +1,23 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class ApplicationSecurityConfig {
+    // Define a SecurityFilterChain bean for HTTP security configurations
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated() // Require authentication for all requests
+                )
+                .httpBasic(httpBasic -> {
+                }); // Configure HTTP Basic authentication
+        return http.build();
     }
-
 }
